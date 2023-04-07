9ea857602b1a4d6e6ae09c80a555a6bd203b4f1e
Fardowsamohamed <muniiramohamed096@gmail.com>
2023-04-08 00:58:56 +0300

    my commit

---
+++ arrays.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class arrays {
+    public static void main(String[] args){
+        int[][]Age={{13,12,7,18},{21,23,27,24}};
+        for(int H=0; H< Age.length;++H){
+            for(int y=0; y < Age[H].length; ++y){
+                System.out.println(Age[H][y]);
+
+            }
+        }
+    }
+}
+
+
+
+
